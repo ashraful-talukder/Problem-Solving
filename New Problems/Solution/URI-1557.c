d5665616327e86ab11e5f744860bfc9ceb798250
ashraful-talukder <ashraful.talukder24@gmail.com>
2021-06-14 17:28:38 +0600

    URI-1557

---
+++ New Problems/Solution/URI-1557.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, i, j, k, c, p, q;
+    while(1){
+        scanf("%d",&N);
+        int arr[N][N];
+        if(N == 0){
+            break;
+        }
+        for (i = 0 ; i < N ; i++){
+            c = 1, k = i;
+            while(k > 0){
+                c = c * 2;
+                k = k - 1;
+            }
+            for(j = 0 ; j < N ; j++){
+                arr[i][j] = c;
+                c = c *2 ;
+            }
+        }
+        p = 0;
+        q = arr[N-1][N-1];
+        while(q > 0){
+            q = q / 10;
+            ++p;
+        }
+        for(i = 0 ; i < N ; i++){
+            for(j = 0; j < N; j++){
+                printf("%*d ", p, arr[i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
